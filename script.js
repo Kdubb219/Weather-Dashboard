370e41043b534f395b355784f518bd2c2c04feca
Kyla <kewise13@gmail.com>
2023-04-22 17:02:24 -0500

    updated index.html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var apiKey= '7a295d18aad3388e176aeda43659a275';
+var apiKey = '7a295d18aad3388e176aeda43659a275';
+//var apiKey = '27c1810b56000ea7afe81f92045aa6ca';
 
 
 // const userAction = async () => {
@@ -6,28 +7,35 @@ var apiKey= '7a295d18aad3388e176aeda43659a275';
 //     const myJson = await response.json(); //extract JSON from the http response
 //     // do something with myJson
 //   }
-function GetWeather(){
-    var city=$('#txtCity').val();
-    var lat=0;
-    var lon=0;
-    var location =GetLocation(city);
-    var url= 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=' + apiKey;
+function GetWeather() {
+    var city = $('#txtCity').val();
+    var lat = 0;
+    var lon = 0;
+    var location = GetLocation(city);
+    //console.log(location[0]);
 }
-  function GetLocation(address){
-    var url= 'http://api.openweathermap.org/geo/1.0/direct?q=' +address + '&limit=0&appid=' + apiKey;
-    Fetchlocation(url).then(location =>{
-        return location;
-      });
-  }
-    
-    async function FetchLocation(url) {
-        const response = await fetch(url);
-        const myJson = await response.json();
-        return myJson;
-    }
-    async function FetchWeather(url) {
-        const response = await fetch(url);
-        const myJson = await response.json();
-        console.log (myJson);
+function GetLocation(address) {
+    var url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + address + '&limit=0&appid=' + apiKey;
+    return FetchLocation(url).then(location => {
+        var lat =location[0].lat;
+        var lon =location[0].lon;
+        var url= 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=' + apiKey;
+        FetchWeather(url).then(weather=>{
+            console.log(weather);
+            $('#currentCity').html(weather.weather[0].name);
+            // var url2= 'http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid=' + apiKey;
+        });
+    });
+}
 
-    }
+async function FetchLocation(url) {
+    const response = await fetch(url);
+    const myJson = await response.json();
+    return myJson;
+}
+async function FetchWeather(url) {
+    const response = await fetch(url);
+    const myJson = await response.json();
+    console.log(myJson);
+
+}

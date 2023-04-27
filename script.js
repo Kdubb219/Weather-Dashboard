9e591bb8d8c21fd3aa3dfabf599bfe2b544980d6
Kyla <kewise13@gmail.com>
2023-04-27 18:37:35 -0500

    new changes

--- script.js
+++ script.js
@@ -13,19 +13,38 @@ function GetWeather() {
     var lon = 0;
     var location = GetLocation(city);
     //console.log(location[0]);
+    $('.cities').append("<div class='citybutton' onclick='GetWeatherbyCity('" + city + "')'>" + city + "</div>");
+    return location;
 }
+function GetWeatherbyCity(city){
+    var lat = 0;
+    var lon = 0;
+    var location = GetLocation(city);
+    //console.log(location[0]);
+    return location;
+}
+
+
+
 function GetLocation(address) {
     var url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + address + '&limit=0&appid=' + apiKey;
-    return FetchLocation(url).then(location => {
-        var lat =location[0].lat;
-        var lon =location[0].lon;
-        var url= 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=' + apiKey;
-        FetchWeather(url).then(weather=>{
-            console.log(weather);
-            $('#currentCity').html(weather.weather[0].name);
-            // var url2= 'http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid=' + apiKey;
+    return FetchLocation(url)
+        .then(location => {
+            var lat = location[0].lat;
+            var lon = location[0].lon;
+            var url = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+            FetchWeather(url)
+                .then(weather => {
+                    console.log(weather.main.temp);
+                    console.log(weather);
+                    $('.city-name').html(weather.name);
+                    $('.weather-description').html(weather.weather[0].description);
+                    $('.temperature').html(weather.main.temp + "°F");
+
+                    $('.weather-icon').attr('src', "http://openweathermap.org/img/wn/" + weather.weather[0].icon + "@4x.png");
+                    // var url2= 'http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid=' + apiKey;
+                });
         });
-    });
 }
 
 async function FetchLocation(url) {
@@ -37,5 +56,6 @@ async function FetchWeather(url) {
     const response = await fetch(url);
     const myJson = await response.json();
     console.log(myJson);
+    return myJson;
 
 }
